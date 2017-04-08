fdd10f6a623dff76aebe973dbf31697371ca1746
Julieta Salvadó <julieta.salvado@gmail.com>
2017-04-08 01:20:01 -0300

    Initial project setup

---
+++ src/main/kotlin/main/App.kt
@@ -0,0 +1,14 @@
+package main
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class App {
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(App::class.java, *args)
+}
+
+
